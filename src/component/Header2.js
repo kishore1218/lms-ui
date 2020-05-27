62b9c8961e4ff20091469e3bb022d2df1d296c6c
kishore1218 <45076973+kishore1218@users.noreply.github.com>
2020-05-27 22:37:47 +0530

    header1

--- src/component/Header2.js
+++ src/component/Header2.js
@@ -25,42 +25,29 @@ class Header2 extends GenericComponent{
                 <div>
              
                     {/* <header> */}
-                    <nav className="navbar navbar-static-top navbar-inverse" role="navigation">
-                        <div className="container-fluid ">
-                        
-                            {/* <div className="navbar-header"> */}
-                            <a className="navbar-brand  " href="#"><img src={tlogo} className="img-responsive twsr" alt=""/></a>
-                                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                                    <span className="icon-bar"></span>
-                                    <span className="icon-bar"></span>
-                                    <span className="icon-bar"></span>
-                                </button>
-                                  
-                            {/* </div>   */}
-
-                            <div className="pull-left ">
-                                <ul className="nav navbar-nav ">
-                                <li className="nav-item"><Link to="/dashboard"  className="nav-link"><span className="fa fa-home homeicon "></span></Link></li>
-                                </ul>
-                            </div>
-                            
-                            <div className="pull-right ">
-                                <ul className="nav navbar-nav ">                               
-                               
-                                    <li className="nav-item"><a href="#" onClick={this.logout}  className="nav-link" > <span className="fa fa-user homeprofileicon">{this.state.username}</span> </a></li>
-                                    <li className="nav-item"><a href="#" onClick={this.logout}  className="nav-link" > <span className="fa fa-power-off homelogouticon"></span> </a></li>
-                                   
-                                </ul>
-
-                                {/* <ul class="dropdown-menu">
-                                        <li><a href="/user/preferences"><i class="icon-cog"></i> Profile</a></li>
-                                        <li><a href="/help/support"><i class="icon-envelope"></i> Contact Support</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="#" onClick={this.logout}><i class="icon-off"></i> Logout</a></li>
-                                    </ul> */}
-                            </div>
+                   <nav class="navbar navbar-static-top navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                        <button type="button" className=" navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                        </button>
+                            <a  href="#"><img src={tlogo} className="img-responsive img-rounded twsr " alt=""/></a>
                         </div>
-                    </nav>               
+
+                        <ul class="nav navbar-nav ">
+                            <li ><Link to="/dashboard"  className="nav-link"><span className="fa fa-home homeicon "></span></Link></li>
+                        </ul>
+
+                        {/* <div className="pull-right float-vertical-align"> */}
+                            <ul className="nav navbar-nav navbar-right">                        
+                                <li className="nav-item text-primary "><a href="#"   className="nav-link" >  <span className="fa fa-user homeprofileicon"> {this.state.username}</span> </a></li> 
+                                <li className="nav-item"><a href="#" onClick={this.logout}  className="nav-link" > <span className="fa fa-power-off homelogouticon"></span> </a></li>                                   
+                            </ul>
+                        {/* </div> */}
+                    </div>
+                </nav>         
                 {/* </header> */}
              </div>
         )
